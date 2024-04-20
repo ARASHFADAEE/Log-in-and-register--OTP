e03a2dea38762f2691f83160d1c3efb2b4e97a8b
ARASHFADAEE <mehrdadfadaee4@gmail.com>
2024-04-20 08:27:04 -0700

    refactor otp.php

--- otp.php
+++ otp.php
@@ -1,1 +1,66 @@
-<?php
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <title>login and register with otp</title>
+</head>
+
+<body>
+<div class="container" id="container">
+    <div class="form-container sign-up">
+        <!----------------------------------signup-------------------------------------->
+        <form>
+            <h1>Create Account</h1>
+
+            <span>or use your email to registration</span>
+            <input type="text" placeholder="Name">
+            <input type="email" placeholder="Email">
+            <input type="text" placeholder="phone">
+            <input type="password" placeholder="Password">
+            <button>Sign Up</button>
+        </form>
+        <!----------------------------------end signup-------------------------------------->
+
+    </div>
+    <div class="form-container sign-in">
+        <!----------------------------------Login-------------------------------------->
+
+        <form>
+            <h1>OTP</h1>
+
+            <span>OTP</span>
+            <input type="number" placeholder="enter your Otp Code">
+            <a href="#">Forget your Password?</a>
+
+            <button>Send to mobile</button>
+            <a style="font-weight: bold" href="otp.php">to email </a>
+
+
+
+
+        </form>
+
+        <!----------------------------------End Login-------------------------------------->
+
+    </div>
+    <div class="toggle-container">
+        <div class="toggle">
+            <div class="toggle-panel toggle-left">
+                <h1>Welcome Back!</h1>
+                <p>Enter your Personal details to use all of site features</p>
+                <button class="hidden" id="login">Sign In</button>
+            </div>
+            <div class="toggle-panel toggle-right">
+                <h1>Hello, Friend!</h1>
+                <p>Register with your Personal details to use all of site features</p>
+                <button class="hidden" id="register">Sign Up</button>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+<script src="assets/js/script.js"></script>
+</html>
